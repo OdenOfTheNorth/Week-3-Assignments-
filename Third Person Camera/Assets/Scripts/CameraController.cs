dd1c1db51e8861609d4000d8389c5a66377047b5
OdenOfTheNorth <61025738+OdenOfTheNorth@users.noreply.github.com>
2020-08-31 12:59:46 +0200

    Backbone of thirdperson camera

---
+++ Third Person Camera/Assets/Scripts/CameraController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform CenterPoint;
+    public Camera myCamera;
+    public Transform Player;
+    public float Sensitivity = 100.0f;
+    public float FOV = 15f;
+
+    
+    
+    private bool isPresingRightMouseButton = false;
+    private float xRotation = 0f;
+    private float yRotation = 0f;
+    private float savedFOV = 0f;
+    private float SensitivityIDS = 0;
+
+
+    private void Awake()
+    {
+        savedFOV = myCamera.fieldOfView;
+        SensitivityIDS = Sensitivity / 2;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * Sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        yRotation -= mouseX;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        CenterPoint.transform.rotation = Quaternion.Euler(xRotation,-yRotation,0f);
+
+        if (Input.GetButton("Fire2"))
+        {
+            myCamera.fieldOfView = FOV;
+            Player.transform.rotation = Quaternion.Euler(0f,-yRotation,0f);
+            Sensitivity = SensitivityIDS;
+        }
+        else
+        {
+            myCamera.fieldOfView = savedFOV;
+        }
+
+    }
+}
---
+++ Third Person Camera/Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Transform cam;
+    private float speed = 10f;
+    private float turnSpeed = 100f;
+
+    private Rigidbody rigidbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        
+        Vector3 dir = Vector3.zero;
+
+        dir.y = 0f;
+
+    }
+}
